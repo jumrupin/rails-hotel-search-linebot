0374bdbe1a3573763a81f88a846cf1f67b832b74
jumrupin <junpei12131018@gmail.com>
2021-02-21 00:35:41 +0900

    [add]検索結果をLINEに送信

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -12,11 +12,8 @@ class LineBotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-           client.reply_message(event['replyToken'], message)
+          message = search_and_create_message(event.message['text'])
+          client.reply_message(event['replyToken'], message)
         end
       end
     end
@@ -44,5 +41,19 @@ class LineBotController < ApplicationController
     }
     response = http_client.get(url, query)
     response = JSON.parse(response.body)
+
+    text = ''
+    response['hotels'].each do |hotel|
+      text <<
+        hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+        hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+        "\n"
+    end
+
+    message = {
+      type: 'text',
+      text: text
+    }
+
   end
 end
